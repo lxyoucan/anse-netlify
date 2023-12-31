88ac7d34ca34969f4a5c25793a68dd140db59dcb
lxyoucan <68362970+lxyoucan@users.noreply.github.com>
2024-01-01 00:18:18 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import unocss from 'unocss/astro'
 import solidJs from '@astrojs/solid-js'
 import node from '@astrojs/node'
 import AstroPWA from '@vite-pwa/astro'
-import vercel from '@astrojs/vercel/edge'
-import netlify from '@astrojs/netlify/edge-functions'
+import vercel from '@astrojs/vercel/serverless'
+import netlify from '@astrojs/netlify'
 import disableBlocks from './plugins/disableBlocks'
 
 const envAdapter = () => {
